c305c55a61731c295842974f663d76cc0495af2f
SaiVaradharajan <113639756+SaiVaradharajan@users.noreply.github.com>
2025-05-20 18:38:02 -0500

    fixed error in job_parser

--- job_parser.py
+++ job_parser.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Function to extract key words:
 def extract_keywords(job_description, max_features=35):
-    vector = TfidfVectorizer(stop_words='englidh', max_features=max_features)
+    vector = TfidfVectorizer(stop_words='english', max_features=max_features)
     vector.fit_transform([job_description])
     return vector.get_feature_names_out()
 
