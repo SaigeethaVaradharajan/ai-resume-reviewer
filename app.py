cac15947f0d6295ac50ad4344c5b9f18463fee58
SaiVaradharajan <113639756+SaiVaradharajan@users.noreply.github.com>
2025-05-20 18:50:30 -0500

    updating changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from parser import extract_text
 from job_parser import extract_keywords
-from score import score
+from score import calculate_score
 
 # Title:
 st.set_page_config(page_title = "AI Resume Reviewer", layout="centered")
@@ -16,13 +16,11 @@ if st.button("Generate Review"):
     if pdf_file and job_description.strip():
         resume_text = extract_text(pdf_file)
         keywords = extract_keywords(job_description)
-        score = score(pdf_file, keywords)
+        score = calculate_score(resume_text, keywords)
 
         st.subheader("Results")
         st.write(f"**Compatibility Score:** {score}%")
-        st.write("**Extracted Keywords:".join(keywords))
-
-    else:
-        st.warning("Error! Upload a redsume and enter a job description")
+        st.write("**Extracted Keywords:**")
+        st.write(",".join(keywords))
 
 
--- score.py
+++ score.py
@@ -1,6 +1,6 @@
 # Calculates compatibility score between job description and uploaded resume
 
-def score(resume, keywords):
-    resume = resume.lower()
-    matches = sum(1 for k in keywords if k in resume)
+def calculate_score(text, keywords):
+    text = text.lower()
+    matches = sum(1 for k in keywords if k in text)
     return round((matches/len(keywords)) * 100, 2)
